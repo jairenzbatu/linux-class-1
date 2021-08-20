2cdb3629529db1af0d4897ca3db0dba4caea61f6
Jairenz Batu <jairenz.batu@dotr.gov.ph>
2021-08-20 15:41:32 +0800

    Updated Prompts

--- module1/vm-setup.sh
+++ module1/vm-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+clear
 echo "Please input your Host IP"
 read vm_input
 ip_detect=$(hostname -I)
@@ -8,14 +8,31 @@ ip_detect=$(hostname -I)
 octet_input=${vm_input%.*}
 octet_detect=${ip_detect%.*}
 
+#colors
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 #output
 # echo "IP Input: ${octet_input}"
 # echo "IP Detect: ${octet_detect}"
 
 if [[ ${octet_detect} == ${octet_input} ]]; then
-	echo "IP Is Within Range"
+	clear
+	echo "#############################################"
+	echo "#                                           #"
+	echo "#           IP Is Within Range              #"
+	echo "#                                           #"
+	echo "#############################################"
+
 else
-	echo "IP Not Within Range."
-	echo "Your IP is ${ip_detect}"
-	echo "While Host IP is ${vm_input}"
+	clear
+	echo "#############################################"
+	echo "#                                           #"
+	echo "#           IP Not Within Range             #"
+	echo "#                                           #"
+	echo "#############################################"
+	echo ""
+	echo -e "Your IP is: ${RED} ${ip_detect} ${NC}"
+	echo -e "While Host IP is:${RED} ${vm_input} ${NC}"
+	echo ""
 fi
